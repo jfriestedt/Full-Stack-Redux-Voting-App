c3c1d913555917c20e333af241cf384864802764
Jon Friestedt <jfrieste@gmail.com>
2016-10-17 16:47:49 -0400

    Add voting behavior tests, add #getWinners to core

--- voting-server/test/core_spec.js
+++ voting-server/test/core_spec.js
@@ -37,7 +37,9 @@ describe('application logic', () => {
           'Sunshine'
         )
       });
+
       const nextState = next(state);
+
       expect(nextState).to.equal(Map({
         vote: Map({
           pair: List.of('Trainspotting', '28 Days Later')
@@ -45,6 +47,50 @@ describe('application logic', () => {
         entries: List.of('Sunshine')
       }));
 
+    });
+
+    it('puts winner of current vote back to entries', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({
+            'Trainspotting': 4,
+            '28 Days Later': 2
+          })
+        }),
+        entries: List.of('Sunshine', 'Millions', '127 Hours')
+      });
+
+      const nextState = next(state);
+
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Sunshine', 'Millions')
+        }),
+        entries: List.of('127 Hours', 'Trainspotting')
+      }));
+    });
+
+    it('puts both from tied vote back to entries', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 Days Later'),
+          tally: Map({
+            'Trainspotting': 2,
+            '28 Days Later': 3
+          })
+        }),
+        entries: List.of('Sunshine', 'Millions', '127 Hours')
+      });
+
+      const nextState = next(state);
+
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Sunshine', 'Millions')
+        }),
+        entries: List.of('127 Hours', 'Trainspotting', '28 Days Later')
+      }));
     });
 
   });
