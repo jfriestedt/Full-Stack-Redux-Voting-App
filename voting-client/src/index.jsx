1dbbfd005855acdd06ad4c0cec90be09ce1779eb
Jon Friestedt <jfrieste@gmail.com>
2016-11-10 16:17:45 -0500

    Voting Client: Add react test utils, add voting button rendering test

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Voting from '../components/Voting';
+import Voting from './components/Voting';
 
 const pair = ['Trainspotting', '28 Days Later'];
 
