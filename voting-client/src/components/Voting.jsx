cfd8885a0849c701b303aef75f1003974dcde85c
Jon Friestedt <jfrieste@gmail.com>
2016-11-11 16:26:19 -0500

    Voting client: Fix typo in Voting component

--- voting-client/src/components/Voting.jsx
+++ voting-client/src/components/Voting.jsx
@@ -7,7 +7,7 @@ export default React.createClass({
   mixins: [PureRenderMixin],
   render: function() {
     return <div>
-      this.props.winner ?
+      {this.props.winner ?
         <Winner ref="winner" winner={this.props.winner} /> :
         <Vote {...this.props} />}
     </div>;
