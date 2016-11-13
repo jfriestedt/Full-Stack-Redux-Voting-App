f38f18ebbf23d923d5beb08397184fd79178f259
Jon Friestedt <jfrieste@gmail.com>
2016-11-13 12:04:22 -0500

    Voting client: Fix typo in Results spec

--- voting-client/test/components/Results_spec.jsx
+++ voting-client/test/components/Results_spec.jsx
@@ -12,7 +12,7 @@ import {expect} from 'chai';
 describe('Results', () => {
 
   it('renders entries with vote counts or zero', () => {
-    const pair = List.of('Trainspotting,', '28 Days Later');
+    const pair = List.of('Trainspotting', '28 Days Later');
     const tally = Map({'Trainspotting': 5});
     const component = renderIntoDocument(
      <Results pair={pair} tally={tally} />
