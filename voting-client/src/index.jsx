724667ee3fb0a82e573ab8d7e6cb875f33bd9799
Jon Friestedt <jfrieste@gmail.com>
2016-11-13 13:15:31 -0500

    Voting client: Connect Results component to Redux store

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -6,7 +6,7 @@ import {Provider} from 'react-redux';
 import reducer from './reducer';
 import App from './components/App';
 import {VotingContainer} from './components/Voting';
-import Results from './components/Results';
+import {ResultsContainer} from './components/Results';
 
 const store = createStore(reducer);
 store.dispatch({
@@ -22,7 +22,7 @@ store.dispatch({
 const pair = ['Trainspotting', '28 Days Later'];
 
 const routes = <Route component={App}>
-  <Route path="/results" component={Results} />
+  <Route path="/results" component={ResultsContainer} />
   <Route path="/" component={VotingContainer} />
 </Route>;
 
