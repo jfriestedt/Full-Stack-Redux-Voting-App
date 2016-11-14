16589062402fbc49b86b010ff88e004da4d61020
Jon Friestedt <jfrieste@gmail.com>
2016-11-14 11:38:19 -0500

    Adjust server configs

--- voting-client/webpack.config.js
+++ voting-client/webpack.config.js
@@ -2,7 +2,7 @@ var webpack = require('webpack');
 
 module.exports = {
   entry: [
-    'webpack-dev-server/client?http://localhost:8080',
+    'webpack-dev-server/client?http://localhost:8090',
     'webpack/hot/only-dev-server',
     './src/index.jsx'
   ],
@@ -25,6 +25,7 @@ module.exports = {
     contentBase: './dist',
     hot: true
   },
+  devtool: 'source-map',
   plugins: [
     new webpack.HotModuleReplacementPlugin()
   ]
